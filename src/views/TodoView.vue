1c69749a38da03babec96f1649ea8c873a66c742
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-21 19:53:27 +0200

    delete task

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -29,6 +29,16 @@
               </v-list-item-title>
               <v-list-item-subtitle>Username here</v-list-item-subtitle>
             </v-list-item-content>
+
+            <!-- Delete button -->
+            <v-list-item-action>
+              <v-btn
+                @click.stop="deleteTask(task.id)"
+                icon
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -65,6 +75,9 @@ export default {
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id)[0];
       task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
