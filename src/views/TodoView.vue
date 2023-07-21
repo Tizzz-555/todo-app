dce93df8f427e8423e6be71fbc8677b1b932dc91
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-21 22:09:57 +0200

    add task

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
     <v-list
       class="pt-0"
       two-line
@@ -52,26 +63,36 @@ export default {
   name: "Home",
   data() {
     return {
+      newTaskTitle: "",
       tasks: [
-        {
-          id: 1,
-          title: "Wake up",
-          done: false,
-        },
-        {
-          id: 2,
-          title: "Get bananas",
-          done: false,
-        },
-        {
-          id: 3,
-          title: "Eat bananas",
-          done: false,
-        },
+        // {
+        //   id: 1,
+        //   title: "Wake up",
+        //   done: false,
+        // },
+        // {
+        //   id: 2,
+        //   title: "Get bananas",
+        //   done: false,
+        // },
+        // {
+        //   id: 3,
+        //   title: "Eat bananas",
+        //   done: false,
+        // },
       ],
     };
   },
   methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false,
+      };
+      this.tasks.push(newTask);
+      this.newTaskTitle = "";
+    },
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id)[0];
       task.done = !task.done;
