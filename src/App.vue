69ffc8060b293e490f565ffd1a29efa4d5b1f1ea
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-21 15:49:13 +0200

    setup drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Vuetify Todo </v-list-item-title>
+          <v-list-item-subtitle> Mattia Beccari </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -18,6 +47,13 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-view-dashboard" },
+      { title: "Completati", icon: "mdi-image" },
+      { title: "Albo", icon: "mdi-help-box" },
+    ],
+  }),
 };
 </script>
