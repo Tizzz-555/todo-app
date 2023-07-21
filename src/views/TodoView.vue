488ef0cf4afac47a250f9fb25f5ac5a1041df88d
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-21 18:16:32 +0200

    create list of tasks

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <v-list
+      two-line
+      flat
+    >
+      <v-list-item>
+        <template v-slot:default="{ active }">
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="active"
+              color="primary"
+            ></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Notifications</v-list-item-title>
+            <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
