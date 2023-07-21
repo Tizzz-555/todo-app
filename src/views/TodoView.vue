99d2c071ab7b37fcfdb728c2605ed1629a7c00bb
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-21 18:35:52 +0200

    add a data property for the tasks

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="home">
     <v-list
+      class="pt-0"
       two-line
       flat
     >
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox
-              :input-value="active"
-              color="primary"
-            ></v-checkbox>
-          </v-list-item-action>
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Wake up</v-list-item-title>
-            <v-list-item-subtitle>Username here</v-list-item-subtitle>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-subtitle>Username here</v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
     </v-list>
   </div>
 </template>
@@ -26,5 +33,23 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake up",
+        },
+        {
+          id: 2,
+          title: "Get bananas",
+        },
+        {
+          id: 3,
+          title: "Eat bananas",
+        },
+      ],
+    };
+  },
 };
 </script>
