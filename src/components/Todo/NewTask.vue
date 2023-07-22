3661affe0d0b77070c6d9b841e06147a0e5afafd
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-22 17:50:20 +0200

    no task component

--- src/components/Todo/NewTask.vue
+++ src/components/Todo/NewTask.vue
@@ -44,5 +44,3 @@ export default {
   props: ["task"],
 };
 </script>
-
-<style></style>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -4,18 +4,7 @@
 
     <list-tasks v-if="$store.state.tasks.length" />
 
-    <div
-      v-else
-      class="no-tasks"
-    >
-      <v-icon
-        size="100"
-        color="primary"
-      >
-        mdi-check-decagram
-      </v-icon>
-      <div class="text-h5 primary--text">No tasks</div>
-    </div>
+    <no-tasks v-else />
   </div>
 </template>
 
@@ -26,19 +15,7 @@ export default {
   components: {
     "field-add-task": require("@/components/Todo/FieldAddTask.vue").default,
     "list-tasks": require("@/components/Todo/ListTasks.vue").default,
+    "no-tasks": require("@/components/Todo/NoTasks.vue").default,
   },
 };
 </script>
-
-<style lang="sass">
-.no-tasks
-  position: absolute
-  left: 50%
-  top: 50%
-  transform: translate(-50%, -50%)
-  opacity: 0.5
-
-@media (max-height: 500px)
-  .no-tasks
-    display: none
-</style>
