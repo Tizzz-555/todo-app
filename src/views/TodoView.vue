120f89a32f82b4800877f49d2c72f502d4143ebb
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-22 16:27:15 +0200

    field add component

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="home">
-    <v-text-field
-      v-model="newTaskTitle"
-      @click:append="addTask"
-      @keyup.enter="addTask"
-      class="pa-3"
-      outlined
-      label="Aggiungi nota"
-      append-icon="mdi-plus"
-      hide-details
-      clearable
-    ></v-text-field>
-
+    <field-add-task />
     <!-- Alert message -->
     <v-alert
       v-if="showAlert"
@@ -88,22 +77,9 @@
 <script>
 export default {
   name: "Home",
-  data() {
-    return {
-      newTaskTitle: "",
-      showAlert: false, // new data property to control the alert
-    };
-  },
-  methods: {
-    addTask() {
-      if (this.$store.state.user) {
-        this.$store.commit("addTask", this.newTaskTitle);
-        this.newTaskTitle = "";
-        this.showAlert = false; // hide the alert if a task is successfully added
-      } else {
-        this.showAlert = true; // show the alert if a user isn't set up
-      }
-    },
+
+  components: {
+    "field-add-task": require("@/components/Todo/FieldAddTask.vue").default,
   },
 };
 </script>
