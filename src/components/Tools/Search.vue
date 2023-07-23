7c0e242a659885c66212195a9bebe65c631202c3
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-23 10:02:46 +0200

    open on focus, close on blur

--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -1,6 +1,9 @@
 <template>
   <v-text-field
-    class="expanding-search mt-1 closed"
+    @focus="searchClosed = false"
+    @blur="searchClosed = true"
+    class="expanding-search mt-1"
+    :class="{ closed: searchClosed }"
     placeholder="Cerca"
     prepend-inner-icon="mdi-magnify"
     filled
@@ -10,7 +13,13 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      searchClosed: true,
+    };
+  },
+};
 </script>
 
 <style lang="sass">
