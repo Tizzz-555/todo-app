6c7bfd55bbc335465b44dbc05784570167f31f4c
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-23 11:11:31 +0200

    create a getter to filter task (by title or user)

--- src/store/index.js
+++ src/store/index.js
@@ -104,5 +104,16 @@ export default new Vuex.Store({
       commit("showSnackbar", "Data modificata!");
     },
   },
-  getters: {},
+  getters: {
+    tasksFiltered(state) {
+      if (!state.search) {
+        return state.tasks;
+      }
+      return state.tasks.filter(
+        (task) =>
+          task.title.toLowerCase().includes(state.search.toLowerCase()) ||
+          task.user.toLowerCase().includes(state.search.toLowerCase())
+      );
+    },
+  },
 });
