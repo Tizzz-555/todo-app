760d095e3e661a995a4f7a9925405e0cdf977365
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-23 19:13:42 +0200

    due date persists

--- src/store/index.js
+++ src/store/index.js
@@ -112,8 +112,15 @@ export default new Vuex.Store({
         });
     },
     updateTaskDueDate({ commit }, payload) {
-      commit("updateTaskDueDate", payload);
-      commit("showSnackbar", "Data modificata!");
+      db.collection("tasks")
+        .doc({ id: payload.id })
+        .update({
+          dueDate: payload.dueDate,
+        })
+        .then(() => {
+          commit("updateTaskDueDate", payload);
+          commit("showSnackbar", "Data modificata!");
+        });
     },
     getTasks({ commit }) {
       db.collection("tasks")
