56c65ad31c970f895b1a90f9230d1365344d10aa
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-24 10:44:26 +0200

    another edit to about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about pa-6">
     <h1>About {{ $store.state.appTitle }}</h1>
-    <p>Una semplice Todo app, creata da Mattia Beccari con Vue 2 e Vuetify.</p>
+    <p>
+      Una semplice Todo app, creata da Mattia Beccari con Vue 2, Vuex e Vuetify.
+    </p>
   </div>
 </template>
